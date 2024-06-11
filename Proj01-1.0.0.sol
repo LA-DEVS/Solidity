bf13e8b1df9316b520a02018e987a8f71a2a77a4
LADEVS <L_A_DEVS@proton.me>
2024-06-11 15:45:08 +0200

    Update Proj01-1.0.0.sol

--- Proj01-1.0.0.sol
+++ Proj01-1.0.0.sol
@@ -1,1 +1,21 @@
+// Kunden Datenbank mit Abruf-Funktion
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// Konstruktor für den Kunden
+struct Customer{ uint id; string name; uint dateOfBirth; uint social; uint status; }
+
+//Status für die Angabe der Daten
+uint constant active = 1; uint constant pending = 2; uint constant deleted = 3;
+
+//Data-Warehouse / Array
+Contract DatabaseExample{
+mapping (uint => Customer) customers;
+customers[key];
+public uint count = 0;
+
+}
 
